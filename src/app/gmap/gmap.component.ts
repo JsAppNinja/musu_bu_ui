6d36303d433a8fe58d82718e8b7efb60b6618780
Connor Barrett <connor.barrett.dev@gmail.com>
2018-11-28 20:52:13 -0500

    55 - Map - Created 'teaser' view of the Map page to encourage users to sign up for the Large plan. Only shows for users on medium and below plans. Created a mobile version of the teaser image.

--- src/app/gmap/gmap.component.ts
+++ src/app/gmap/gmap.component.ts
@@ -7,6 +7,7 @@ import { TagsService } from '../services/tags.service';
 import { MatSort, MatDialog, MatChipInputEvent, MatSlideToggleChange, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { AgmCoreModule, GoogleMapsAPIWrapper, AgmMap, LatLngBounds, LatLngBoundsLiteral } from '@agm/core';
 import { UserService } from '../services/user.service';
+import { AuthService } from '../services/auth.service';
 
 declare var google: any;
 
@@ -38,6 +39,7 @@ export class GmapComponent implements OnInit, AfterViewInit {
   checked;
 
   user;
+  subscriptionPlan;
   queryName;
   description;
   isFormInvalid: boolean;
@@ -53,6 +55,7 @@ export class GmapComponent implements OnInit, AfterViewInit {
     private route: ActivatedRoute,
     public dialog: MatDialog,
     public userService: UserService,
+    private authService: AuthService,
   ) { }
 
   ngOnInit() {
@@ -81,6 +84,38 @@ export class GmapComponent implements OnInit, AfterViewInit {
         }
       }
     });
+
+
+    this.user = JSON.parse(localStorage.getItem("profile"));
+    var isAuthenticated = this.authService.isAuthenticated();
+    let self = this;
+
+    if (isAuthenticated) {
+      this.userService.getUserByEmail(this.user.email)
+        .then(result => {
+          this.subscriptionPlan = result[0].subscriptionPlan;
+        });
+    } else {
+      self.authService.lock.on("authenticated", function(authResult) {
+        self.authService.lock.getUserInfo(authResult.accessToken, function(error, profile) {
+          if (error) {
+            return;
+          }
+          self.userService.getUserByEmail(profile.email)
+            .then(result => {
+              self.subscriptionPlan = result[0].subscriptionPlan
+            });
+          self.userService.subscribeWithMailChimps(profile).then(
+            response => {
+              //User created on MailChimps
+            },
+            error => {
+              //Error creating user on MailChimps
+            }
+          );
+        });
+      });
+    }
   }
 
   ngAfterViewInit() {
